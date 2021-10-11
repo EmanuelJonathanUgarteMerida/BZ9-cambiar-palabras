f2d1ff725da5d305e4f85703d09cb76ed91d8fee
Emanuel Jonathan Ugarte Mérida <emanuelugartemerida@gmail.com>
2021-10-11 22:06:08 +0200

    se usan inputs en vez de sys

--- cambiaPalabras.py
+++ cambiaPalabras.py
@@ -1,27 +1,19 @@
-import sys
 import os
 
-argumentos = sys.argv
-# Si la lista longitud de sys.argv es menor de cuatro terminamos, de lo contrario calcular
+_FICHERO = input("Fichero: ")
 
-if len(argumentos) == 4:
+# verificamos si existe el archivo
+if os.path.isfile(_FICHERO):
+    palabraOriginal = input("Palabra vieja: ")
+    palabraNueva = input("Palabra nueva: ")
+    fichero = open(_FICHERO, 'r')
+    original = fichero.read()
+    fichero.close()
+    nueva = original.replace(palabraOriginal, palabraNueva)
 
-    _FICHERO = argumentos[1]
-    palabraOriginal = argumentos[2]
-    palabraNueva = argumentos[3]
-    # verificamos si existe el archivo
-    if os.path.isfile(_FICHERO):
-        fichero = open(_FICHERO, 'r')
-        original = fichero.read()
-        fichero.close()
-        nueva = original.replace(palabraOriginal, palabraNueva)
-
-        fichero = open(_FICHERO, 'w')
-        fichero.write(nueva)
-        fichero.close()
-        print("Listo!")
-    else:
-        print("No existe el fichero {} en el directorio".format(_FICHERO))
+    fichero = open(_FICHERO, 'w')
+    fichero.write(nueva)
+    fichero.close()
+    print("Listo!")
 else:
-    print("Ayuda")
-    print("\tpython cambia palabras.py <nombrefichero> <textoOriginal> <textoNuevo>")
+    print("No existe el fichero {} en el directorio".format(_FICHERO))
