0d9bcc0473dcfe1d617a4f9f1d1c1a2e42404cd5
Emanuel Jonathan Ugarte Mérida <emanuelugartemerida@gmail.com>
2021-10-11 20:56:26 +0200

    otro commit con el replace

--- cambiaPalabras.py
+++ cambiaPalabras.py
@@ -1,9 +1,12 @@
-fichero = open('fichero.txt', 'r')
+_FICHERO = 'fichero.txt'
+palabraOriginal = 'yesos'
+palabraNueva = 'tizas'
+fichero = open(_FICHERO, 'r')
 original = fichero.read()
 fichero.close()
-nueva = original.replace('tizas', 'yesos')
+nueva = original.replace(palabraOriginal, palabraNueva)
 
 
-fichero= open('fichero.txt','w')
+fichero = open('fichero.txt', 'w')
 fichero.write(nueva)
 fichero.close()
